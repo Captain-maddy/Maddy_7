8b108596d0d6dab7cd0d590a63cf626bf61a1fe7
Captain-maddy <saravinthan23@gmail.com>
2025-03-12 14:10:53 +0530

    commands added

---
+++ commands/aswin.java
@@ -0,0 +1,12 @@
+public class aswin{
+    public static void main(String args[])
+    {
+        
+        int numone=67;// initialise the variable numone
+        System.out.println(numone); // print the variable
+        System.out.println();
+
+
+
+    }
+}
