93f48762bd658e83f445bda2a9e205e809b70439
Captain-maddy <saravinthan23@gmail.com>
2025-03-12 14:17:36 +0530

    multiline commands are written

--- commands/aswin.java
+++ commands/aswin.java
@@ -1,7 +1,14 @@
 public class aswin{
     public static void main(String args[])
     {
-        
+        /*
+        1) create a commands folder
+        2)create aswin file
+        3)initialise class aswin
+        4)intialise variable numone
+        5)print the variable numone
+         
+         */
         int numone=67;// initialise the variable numone
         System.out.println(numone); // print the variable
         System.out.println();
