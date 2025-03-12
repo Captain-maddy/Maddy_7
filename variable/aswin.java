b8e799463b95ff27a61c7faaad550a1cd50cd3c2
Captain-maddy <saravinthan23@gmail.com>
2025-03-12 13:52:47 +0530

    variable finished

---
+++ variable/aswin.java
@@ -0,0 +1,12 @@
+public class aswin{
+    public static void main(String args[])
+    {
+        
+        int numone=67;
+        System.out.println(numone);
+        System.out.println();
+
+
+
+    }
+}
