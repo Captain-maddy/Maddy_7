659ced93f4d21b5a5b9a8e1f6f674699922aa062
Captain-maddy <saravinthan23@gmail.com>
2025-03-12 15:10:24 +0530

    return type

---
+++ returntype/ret1.java
@@ -0,0 +1,16 @@
+class ret1{
+    public static void main(String[] args) {
+        ret1 obj=new ret1();
+        obj.add1();
+        System.out.println();
+        
+       
+    }
+    int add1(){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        return c;
+
+    }
+}
