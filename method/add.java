5f1de9dfb90f8284bb8f198174971708cfd2ca93
Captain-maddy <saravinthan23@gmail.com>
2025-03-12 14:26:51 +0530

    additiion completed

---
+++ method/add.java
@@ -0,0 +1,17 @@
+public class add{
+    
+    void add(){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("addition is "+c);
+    }
+    
+    
+    public static void main(String[] args) {
+        add obj=new add();
+        obj.add();
+        
+    }
+  
+}
