b6c9d6868d18ded2982c4b4cf85839fe51a2c8d5
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 11:05:27 +0530

    major or minor

---
+++ mojorminor/agecheck.java
@@ -0,0 +1,14 @@
+public class agecheck{
+    public  static void main(String[] args){
+        int age=54;
+        if(age>=18)
+        {
+            System.out.println("he is major");
+        }
+        else{
+            System.out.println("he is minor");
+        }
+
+    }
+
+}
