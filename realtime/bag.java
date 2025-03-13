85277a45a8a4f9f9c28ce53030a1916d206396d1
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 12:17:35 +0530

    bag

---
+++ realtime/bag.java
@@ -0,0 +1,12 @@
+public class bag {
+    public static void main(String[] args) {
+        String bag[]={"red","blue","green"};
+        for(int i=0;i<3;i++){
+            if(bag[i]=="red"){
+                System.out.println(bag[i]);
+            }
+        }
+        
+        
+    }    
+}
