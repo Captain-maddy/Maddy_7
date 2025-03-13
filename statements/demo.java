a96bd191856591b659441c3c495bbd5c74af750a
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 14:07:49 +0530

    for loop

---
+++ statements/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        int a[]={10,20};
+        int n=3;
+        for(int i=0;i<n;i++){
+        System.out.println(a[i]);
+        }
+    }
+}
