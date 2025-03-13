07a9ba2345729dc881666567650920a5980870d8
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 10:12:32 +0530

    add of doubles

---
+++ doub/dou_add.java
@@ -0,0 +1,10 @@
+class dou_add{
+    public static void main(String[] args) {
+        double a= 10.3342;
+        double b=10.32234;
+        double c=a+b;
+        System.out.println(c);
+    
+    
+    }
+}
