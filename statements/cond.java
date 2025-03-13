11e2fad57a43407fefb27e25315c304ffca2c001
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 10:51:20 +0530

    add of doubles

--- statements/cond.java
+++ statements/cond.java
@@ -1,11 +1,12 @@
 public class cond{
     public  static void main(String[] args){
         boolean a=true;
+        boolean b=false;
         if(a)
         {
             System.out.println("this is true ");
         }
-        if(a){
+        if(b){
             System.out.println("This is false");
         }
 
