86dbbc69b62852dd320e4d73058360fe886eb6da
Captain-maddy <saravinthan23@gmail.com>
2025-03-13 10:46:04 +0530

    if statement

---
+++ statements/cond.java
@@ -0,0 +1,13 @@
+public class cond{
+    public  static void main(String[] args){
+        boolean a=true;
+        if(a)
+        {
+            System.out.println("this is true ");
+        }
+        else{
+            System.out.println("This is false");
+        }
+
+    }
+}
