7125e0738dfbab0eb13aeb1dd11ff32d8453f751
Captain-maddy <saravinthan23@gmail.com>
2025-03-14 14:14:22 +0530

    break

---
+++ statement2/break1.java
@@ -0,0 +1,17 @@
+public class break1{
+    public static  void main(String[] args){
+        int i=0;
+        while (i<=10) { 
+            if(i%2==0){
+                if(i>5){
+                    break;
+                }
+                
+                System.out.println(i);
+            }
+            i=i+1;
+
+            
+        }
+    }
+}
