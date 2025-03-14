bfdaaed84bee477ca18df0b41f68c731a2c8dc6a
Captain-maddy <saravinthan23@gmail.com>
2025-03-14 10:30:12 +0530

    vowels

---
+++ vowels/vowel.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class vowel{
+    public static void main(String[] args) {
+        System.out.println("enter a alphabet");
+        Scanner obj=new Scanner(System.in);
+        String m=obj.nextLine();
+        if(m=="a"){
+            System.out.println("This is vowel ");
+
+        }
+        if(m=="e"){
+            System.out.println("This is vowel ");
+
+        }
+        if(m=="i"){
+            System.out.println("This is vowel ");
+
+        }
+        if(m=="o"){
+            System.out.println("This is vowel ");
+
+        }
+        if(m=="u"){
+            System.out.println("This is vowel ");
+
+        }
+        else{
+            System.out.println("This is constant");
+        }
+        
+        
+        
+    
+        
+        
+        
+        
+        
+     
+       }
+    
+}
